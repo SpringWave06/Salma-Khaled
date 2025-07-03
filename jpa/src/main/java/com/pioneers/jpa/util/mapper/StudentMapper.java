61b72f613ac592f189544961d558be0c89707074
Salma <salma.khaled963@gmail.com>
2025-07-03 21:14:12 +0300

    Reapply "TODO JPA & Test"
    This reverts commit 00332f184fe189e17b747f8604712c2b564a439b.

--- jpa/src/main/java/com/pioneers/jpa/util/mapper/StudentMapper.java
+++ jpa/src/main/java/com/pioneers/jpa/util/mapper/StudentMapper.java
@@ -4,6 +4,7 @@ import com.pioneers.jpa.model.dto.StudentDto;
 import com.pioneers.jpa.model.entity.Student;
 import lombok.experimental.UtilityClass;
 
+import java.util.List;
 import java.util.UUID;
 
 @UtilityClass
@@ -14,6 +15,11 @@ public class StudentMapper {
         String firstName = names[0];
         String secondName = names[1];
 
-        return new StudentDto(firstName, secondName, student.getEmail(), student.getPhone(), student.getEnrollmentDate(), student.getDegree(), student.getGender());
+        return new StudentDto(firstName, secondName, student.getEmail(), student.getPhone(), student.getEnrollmentDate(), student.getDegree(), student.getGender(), student.getAge());
+    }
+
+    public static List<StudentDto> toListStudentDtos(List<Student> students){
+        List<StudentDto> ListStudentDtos = students.stream().map(student -> toStudentDto(student)).toList();
+        return ListStudentDtos;
     }
 }
